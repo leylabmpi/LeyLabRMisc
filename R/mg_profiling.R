ecc2a31ad7f0c913f1f659c4e72c4bb181b50de4
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2020-04-07 10:33:26 +0200

    simplified read_bracken

--- R/mg_profiling.R
+++ R/mg_profiling.R
@@ -6,25 +6,17 @@
 #' tidytable (w/ data.table) used to speed the process up.
 #'
 #' @param infile Path to bracken table file
-#' @param is_gzip Is the table file gzip'ed?
-#' @param n_lines Number of lines to read. If < 1, all lines will be read.
+#' @param nrows Number of table rows to read. If Inf, all lines will be read.
 #' @param keep_frac If TRUE, keep all columns ending in "_frac"; otherwise, keep "_num" columns.
 #' @param tax_levs Taxonomic levels to separate the taxonomy column into.
 #' @return data.table
-read_bracken = function(infile, is_gzip=FALSE, n_lines=0, keep_frac=TRUE,
+read_bracken = function(infile, nrows=Inf, keep_frac=TRUE,
                         tax_levs = c('Domain', 'Phylum', 'Class', 'Order',
                                      'Family', 'Genus', 'Species')){
   if(n_lines > 0){
     n_lines = glue::glue('| head -n {n}', n=n_lines)
   } else {
     n_lines = ''
-  }
-  if(is_gzip){
-    cmd = glue::glue('gunzip -c {file} {n_lines}',
-                     file=infile, n_lines=n_lines)
-  } else {
-    cmd = glue::glue('cat {file} {n_lines}',
-                     file=infile, n_lines=n_lines)
   }
   if(keep_frac){
     to_rm = '_num'
@@ -33,7 +25,7 @@ read_bracken = function(infile, is_gzip=FALSE, n_lines=0, keep_frac=TRUE,
     to_rm = '_frac'
     to_keep = '_num'
   }
-  dt = data.table::fread(cmd=cmd, sep='\t', check.names=TRUE) %>%
+  dt = data.table::fread(infile, sep='\t', nrows=nrows, check.names=TRUE) %>%
     tidytable::dt_select(-taxIDs, -dt_ends_with(!!to_rm)) %>%
     tidytable::dt_mutate(taxonomy = gsub(';[pcofgs]__', ';', taxonomy),
                          taxonomy = gsub('^d__', '', taxonomy)) %>%
