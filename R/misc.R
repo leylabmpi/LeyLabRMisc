c12d10ef381d4340a2eec2bb5e1c762a51e1b5a5
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2020-04-27 16:49:36 +0200

    added taxonomy_levels

---
+++ R/misc.R
@@ -0,0 +1,8 @@
+#' A simple function that returns a vector of taxonomy levels
+#'
+#' This just saves some typing, since I find myself constantly typing out:
+#' c('Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
+taxonomy_levels = function(){
+  x = c('Domain', 'Phylum', 'Class', 'Order', 'Family', 'Genus', 'Species')
+  return(x)
+}
