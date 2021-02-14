5e2dcc2b688087a7e1e189a0f19a38021f09d30e
Nicholas Youngblut <nicholas.youngblut@tuebingen.mpg.de>
2021-02-14 14:47:43 +0100

    updated namespace

--- R/LeyLabRMisc-package.R
+++ R/LeyLabRMisc-package.R
@@ -1,7 +1,8 @@
 # Suppress R CMD check note
-# @import tidytable
-# @import tidyselect
-# @rawNamespace import(rlang, except = `:=`)
-#' @importFrom magrittr `%>%`
-#' @importFrom rlang `:=` .data as_label as_name enquo enquos eval_tidy expr sym syms
+# @importFrom magrittr `%>%`
+# @importFrom rlang `:=` .data as_label as_name enquo enquos eval_tidy expr sym syms
+#' @import tidytable
+#' @import tidyselect
+#' @import data.table
+#' @rawNamespace import(rlang, except = `:=`)
 NULL
